0731181a02aed62f6887ed2a877f209c792c4b2d
ky13R <kyle.ritchie4b5752@gmail.com>
2016-08-08 22:52:13 -0400

    Update hawkularCreate.sh

--- hawkularCreate.sh
+++ hawkularCreate.sh
@@ -66,14 +66,18 @@ oc create -f /tmp/$volName.json
 # Copy example template
 cp /usr/share/openshift/examples/infrastructure-templates/enterprise/metrics-deployer.yaml .
 
+# Get subdmain to use as variable for the following commands
+cloudDomain=$(grep subdomain /etc/origin/master/master-config.yaml | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
+
+
 if [ $volSize != 10 ]; then
   # Cassandra db is expecting a 10G persistent volume; if using a larger volume, you need to specify with the following:
-  oc process -f metrics-deployer.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics-openshift-infra.apps.yourDomain.com, CASSANDRA_PV_SIZE="$volSize"Gi | oc create -f -
+  oc process -f metrics-deployer.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics-openshift-infra.$cloudDomain, CASSANDRA_PV_SIZE="$volSize"Gi | oc create -f -
 else
- oc process -f metrics-deployer.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics-openshift-infra.apps.yourDomain.com | oc create -f -
+ oc process -f metrics-deployer.yaml -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics-openshift-infra.$cloudDomain | oc create -f -
 fi 
 # add the following line to the /etc/origin/master/master-config.yaml under 'assetConfig'
-sed -i s/publicURL/\nmetricsPublicURL: https:\/\/hawkular-metrics-openshift-infra.apps.yourDomain.com\/hawkular\/metrics/'
+sed -i "s/logoutURL:\ ""/logoutURL:\ ""\n  metricsPublicURL:\ https:\/\/hawkular-metrics-openshift-infra.$cloudDomain\/hawkular\/metrics/" /etc/origin/master/master-config.yaml
 
 # Bounce the master/nodes
 echo "Restarting atomic-openshift-master"
